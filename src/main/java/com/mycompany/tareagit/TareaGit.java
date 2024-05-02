4132cb123cf78f4c9bc25b034703db651086da6b
Diana Ortiz <diana.ortiz.miranda@iesjulianmarias.es>
2024-05-02 09:35:39 +0200

    prueba

---
+++ src/main/java/com/mycompany/tareagit/TareaGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareagit;
+
+/**
+ *
+ * @author admin
+ */
+public class TareaGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
